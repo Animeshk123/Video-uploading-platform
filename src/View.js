798cab2895bc10f0fc4aaef304f48b0b25507191
Animesh kumbhakar <75767221+Animeshk123@users.noreply.github.com>
2023-04-15 14:54:44 +0530

    Update View.js

--- src/View.js
+++ src/View.js
@@ -40,7 +40,7 @@ const Views = ({ setProgress }) => {
         <div className="container">
           <VideoWrapper>
             <div className="videoW">
-              <Video controls src={video.videoURL} autoPlay={true} muted={true}></Video>
+              <Video controls src={video.videoURL} autoPlay={true}></Video>
               <p>{video.title}</p>
             </div>
             <div>
